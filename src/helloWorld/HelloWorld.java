b9bb9205cd01ae077607cfe97599b8eb0e460537
carlospedraza <carlospedraza@usco.edu.co>
2024-02-08 12:16:47 -0500

    First update
    First update

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld; //línea
+
+/**
+ *
+ * @author Carlos Pedraza 20241234567
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.print("");
+    }
+}
---
+++ src/primeraApp/HolaMundo.java
@@ -0,0 +1,11 @@
+package primeraApp; //Define el paquete de la clase
+/**
+ *
+ * @author Carlos Pedraza 202412345676
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.print("");
+    }
+}
