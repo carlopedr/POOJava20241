adc244674d41299efac3f3259ac69fc393618e98
carlospedraza <carlospedraza@usco.edu.co>
2024-02-27 18:36:10 -0500

    Update HelloWorld.java

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,12 +1,14 @@
 package helloWorld; //línea
 
-/**
- *
- * @author Carlos Pedraza 20241234567
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print("");
+        saludar();
+    }
+    
+    public static void saludar(){
+        System.out.println("Hello world desde saludar!");
         System.out.print("");
     }
 }
