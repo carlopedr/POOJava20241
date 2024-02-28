02796d8d69cccd98b7a89c79794eb2a12adad487
carlospedraza <carlospedraza@usco.edu.co>
2024-02-28 04:53:43 -0500

    Update métodos

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -5,10 +5,27 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.print("");
         saludar();
+        saludar2("Hello world como parametro!!");
+        int res = sumar(3, 4);
+        saludar(String.valueOf(res));
     }
     
     public static void saludar(){
         System.out.println("Hello world desde saludar!");
         System.out.print("");
+    }
+    public static void saludar2(String mensaje){
+        System.out.println(mensaje);
+    }
+    public static void saludar(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+    public static int sumar(int a,int b){
+        String c1="",c2="";
+        if(c1.equals(c2)){
+            //Cadenas iguales
+        }
+        return a+b;
     }
 }
